2630cf58e26ffa8de59f541d6e0347bccb85b6bd
David Neely <dneely@hawaii.edu>
2017-01-28 16:40:25 -1000

    (miniplayer):Animates the intro of the miniplayer on viewWillAppear

--- playerPlayer/BrowserViewController.swift
+++ playerPlayer/BrowserViewController.swift
@@ -14,6 +14,8 @@ import AVFoundation
 
 class BrowserViewController: UIViewController {
     
+    @IBOutlet weak var miniPlayer: UIView!
+    
     var labelText: String = ""
     
     @IBOutlet weak var textLabel: UILabel!
@@ -26,12 +28,37 @@ class BrowserViewController: UIViewController {
 
     }
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        
-//        var destViewController: ViewController = segue.destination as! ViewController
-//        
-//
-//    }
+    override func viewWillAppear(_ animated: Bool) {
+        
+        let currentX = miniPlayer.frame.origin.x
+        
+        let currentY = miniPlayer.frame.origin.x
+        
+        let updatedY = currentY + 100
+        
+        miniPlayer.frame = CGRect(x: currentX, y: updatedY, width: miniPlayer.frame.width, height: miniPlayer.frame.height)
+
+        // move up the view
+        
+        let options = UIViewAnimationOptions.curveEaseOut
+        
+        UIView.animate(withDuration: 1.0, delay: 0.0, options: options, animations: {
+            
+            self.miniPlayer.frame = CGRect(x: currentX, y: currentY, width: self.miniPlayer.frame.width, height: self.miniPlayer.frame.height)
+            
+        }, completion: { animationFinished in
+            
+            
+        })
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        var destViewController: ViewController = segue.destination as! ViewController
+        
+
+    }
 
 }
  
